4d6f6d765b3ba56a0aedfb6cb8967260c2ead73f
Adaildson Moreira dos Santos <127758538+Adaildson002@users.noreply.github.com>
2023-11-09 10:49:05 -0300

    atualização
    Troca do operador & por AND.
    E adição do código para verificar o menor.

--- ex 33.py
+++ ex 33.py
@@ -1,7 +1,7 @@
 import time
 
 print('_'*50)
-print('Digite tres numeros para sabermos qual é o maior.')
+print('Digite tres numeros para sabermos qual é o maior e menor.')
 time.sleep(2)
 print('_'*50)
 
@@ -10,16 +10,28 @@ primeiro = int(input('Informe o primeiro numero:'))
 segundo = int(input('Informe o segundo numero:'))
 terceiro = int(input('Informe o tercei numero:'))
 
-print ('VERIFICANDO QUAL O MAIOR . . .')
+print ('VERIFICANDO QUAL O MAIOR E O MENOR . . .')
 
 # verifica se todos são iguais, se não damos inicio a verificação do maior
-if primeiro==segundo & primeiro == terceiro:
+if primeiro==segundo and primeiro == terceiro:
     print('TODOS OS NUMEROS SÃO IGUAIS')
 else:
-    if primeiro > segundo & primeiro > terceiro:
-        print(f'O numero {primeiro} é o maior ente os tres')
+    if primeiro > segundo and primeiro > terceiro:
+        print(f'O numero {primeiro} é o maior')
     else:
         if segundo > terceiro:
-            print(f'O numero {segundo} é o maior entre os tres')
+            print(f'O numero {segundo} é o maior')
         else:
-            print(f'O numero {terceiro} é o maior entre os tres')
+            print(f'O numero {terceiro} é o maior')
+
+# verifica se todos são iguais, se não damos inicio a verificação do menor
+if primeiro==segundo and primeiro == terceiro:
+    igual == 'igual'
+else:
+    if primeiro < segundo and primeiro < terceiro:
+        print(f'O numero {primeiro} é o menor')
+    else:
+        if  segundo < terceiro:
+            print(f'O numero {segundo} é o menor')
+        else:
+            print(f'O numero {terceiro} é o menor')
