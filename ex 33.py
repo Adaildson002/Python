18d9cf6e2fc7f592439e61bb73692f8ba70fbb94
Adaildson Moreira dos Santos <127758538+Adaildson002@users.noreply.github.com>
2023-11-09 10:19:10 -0300

    Verificando igualdade
    adicionado IF para verificar se os números são iguais antes de verificar qual o maior

--- ex 33.py
+++ ex 33.py
@@ -2,17 +2,24 @@ import time
 
 print('_'*50)
 print('Digite tres numeros para sabermos qual é o maior.')
-#time.sleep(2)
+time.sleep(2)
 print('_'*50)
-primeiro= int(input('Informe o primeiro numero:'))
-segundo= int(input('Informe o segundo numero:'))
-terceiro=int(input('Informe o tercei nroumero:'))
-print ('Verificando qual o maior')
-#time.sleep(5)
-if primeiro > segundo & primeiro > terceiro:
-    print(f'O numero {primeiro} é o maior ente os tres')
+
+# Recebe os tres numeros infomados
+primeiro = int(input('Informe o primeiro numero:'))
+segundo = int(input('Informe o segundo numero:'))
+terceiro = int(input('Informe o tercei numero:'))
+
+print ('VERIFICANDO QUAL O MAIOR . . .')
+
+# verifica se todos são iguais, se não damos inicio a verificação do maior
+if primeiro==segundo & primeiro == terceiro:
+    print('TODOS OS NUMEROS SÃO IGUAIS')
 else:
-    if segundo > terceiro:
-        print(f'O numero {segundo} é o maior entre os tres')
+    if primeiro > segundo & primeiro > terceiro:
+        print(f'O numero {primeiro} é o maior ente os tres')
     else:
-        print(f'O numero {terceiro} é o maior entre os tres')
+        if segundo > terceiro:
+            print(f'O numero {segundo} é o maior entre os tres')
+        else:
+            print(f'O numero {terceiro} é o maior entre os tres')
